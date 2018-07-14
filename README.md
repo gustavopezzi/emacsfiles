834e1214bc0213821247efac42e5195bd6ee67cc
Gustavo Pezzi <gustavocp@gmail.com>
2018-07-14 09:27:37 +0100

    Added new indentation key mapping.

---
+++ README.md
@@ -0,0 +1,53 @@
+C               ctrl key
+M               meta key (alt or esc)
+
+M-!             execute bash command
+M-x             executes emacs command with auto-complete
+
+C-n             next line down
+C-p             previous line up
+C-f             forward one character right
+C-b             back one character left
+M-f             forward one word right
+M-b             back one word left
+C-v             page-down
+M-v             page-up
+M-<             jump to first line of file
+M->             jump to last line of file
+C-a             home / toggle beginning of line and first non-empty char
+C-e             end / last character of a line
+
+C-x h           select whole file
+C-Shift-right   indent block one space to the right
+C-Shift-left    indent block one space to the left
+C-x C-x         re-select last marked block
+
+C-s             search text forward
+C-r             search text backwards
+C-%             replaces text (replace results using (y)-yes, (n)-no, (!)-repeat for all
+M-x rgrep       find text inside files in a base directory recursively
+M-x find-dired  find files by name in a base directory
+
+C-space         start selection mark
+C-w             cut the selected text
+M-w             copy the selected text
+C-y             paste the text in the clipboard
+
+C-x u           undo batch changes
+
+F6              toggle display line numbers
+F9              toggle display invisibles and whitespace
+F11             toggles fullscreen
+
+C-x C-f         opens a file
+C-x o           jumps to other frame/window
+C-x k           kill current buffer
+C-x C-b         show all buffers ("d" to mark those to be deleted, "x" to remove them)
+
+C-x 1           maximize current one and close all other frames
+C-x 2           open new frame vertically
+C-x 3           open new frame horizontally
+C-x
+C-g             cancel command
+
+C-x-c           exit emacs
--- init.el
+++ init.el
@@ -1,3 +1,6 @@
+;; unset keyboard coding system
+(set-keyboard-coding-system nil)
+
 ;; toggles fullscreen visibility when f11 is pressed
 (defun toggle-fullscreen()
   (interactive)
@@ -12,9 +15,6 @@
   (set-fringe-mode 0)
   (menu-bar-mode -99)
   (toggle-scroll-bar -1))
-
-;; defines custom variables
-(custom-set-variables '(inhibit-startup-screen t))
 
 ;; hides menu bar and scrollbar on init
 (hide-menu-scroll-bar)
@@ -23,98 +23,40 @@
 ;; set truncate lines off
 (set-default 'truncate-lines t)
 
-;; defines font family and size
-(custom-set-faces)(defun fontify-frame (frame)
-(set-frame-parameter frame 'font "Hermit-12"))
-(fontify-frame nil)
-(push 'fontify-frame after-make-frame-functions)
-
 ;; define color theme files
-(push (substitute-in-file-name "~/.emacs.d/colorthemes/borland-blue-theme/") custom-theme-load-path)
-(push (substitute-in-file-name "~/.emacs.d/colorthemes/comidia-theme/") custom-theme-load-path)
-(push (substitute-in-file-name "~/.emacs.d/colorthemes/green-screen-theme/") custom-theme-load-path)
-(load-theme 'green-screen t)
-
-;; show line numbers on init
-(global-linum-mode t)
-
-;; set line number color
-(setq linum-format "%4d \u2502 ")
-
-;; defines tab size
-(setq default-tab-width 4)
-
-;; unset keyboard coding system
-(set-keyboard-coding-system nil)
-
-;; stop autosaving and creating backup files
-(setq make-backup-files nil)
-(setq auto-save-default nil)
-
-;; toggles fullscreen visibility when f11 is pressed
-(defun toggle-fullscreen()
-  (interactive)
-  (when (eq window-system 'x)
-    (set-frame-parameter nil 'fullscreen (when (not (frame-parameter nil 'fullscreen)) 'fullboth))))
-(global-set-key [f11] 'toggle-fullscreen)
-(toggle-fullscreen)
-
-;; hides menubar and scrollbar from the interface
-(defun hide-menu-scroll-bar()
-  (tool-bar-mode -1)
-  (set-fringe-mode 0)
-  (menu-bar-mode -99)
-  (toggle-scroll-bar -1))
-
-;; requires neotree directory and toggles when f8 is pressed
-(add-to-list 'load-path "~/.emacs.d/neotree")
-(require 'neotree)
-(setq neo-window-width 35)
-(setq neo-theme 'ascii)
-(global-set-key [f8] 'neotree-toggle)
-
-;; defines custom variables
-(custom-set-variables
-  '(inhibit-startup-screen t))
-
-;; hides menu bar on init
-(hide-menu-scroll-bar)
-(scroll-bar-mode -1)
-
-;; set truncate lines off
-(set-default 'truncate-lines t)
-
-;; defines font family and size
-(custom-set-faces)
-(defun fontify-frame(frame)
-  (set-frame-parameter frame 'font "Hermit-12"))
-(fontify-frame nil)
-(push 'fontify-frame after-make-frame-functions)
-
-;; define color theme file
 (push (substitute-in-file-name "~/.emacs.d/colorthemes/borland-blue-theme/") custom-theme-load-path)
 (push (substitute-in-file-name "~/.emacs.d/colorthemes/comidia-theme/") custom-theme-load-path)
 (push (substitute-in-file-name "~/.emacs.d/colorthemes/green-screen-theme/") custom-theme-load-path)
 (load-theme 'borland-blue t)
 
-;; show line numbers on init
-(global-linum-mode t)
+;; toggles linum mode when f6 is pressed
+(global-set-key [f6] 'linum-mode)
 
-;; set line number format
+;; defines linum format and border
 (setq linum-format "%4d \u2502 ")
 
-;; defines tab size
+;; defines indentation size
 (setq default-tab-width 4)
 
-;; unset keyboard coding system
-(set-keyboard-coding-system nil)
+;; use spaces instead of tabs when indenting
+(setq-default indent-tabs-mode nil)
 
-;; stop autosaving and creating backup files
-(setq make-backup-files nil)
-(setq auto-save-default nil)
+;; toggles whitespace mode when f9 is pressed
+(global-set-key [f9] 'whitespace-mode)
+
+;; defines custom variables
+(custom-set-variables
+  '(inhibit-startup-screen t))
+
+;; defines font family and size
+(custom-set-faces)
+(defun fontify-frame(frame)
+  (set-frame-parameter frame 'font "Perfect DOS VGA 437-12"))
+(fontify-frame nil)
+(push 'fontify-frame after-make-frame-functions)
 
 ;; toggles first char and first nonwhite char when C-a is pressed
-(defun smarter-move-beginning-of-line (arg)
+(defun smarter-move-beginning-of-line(arg)
   (interactive "^p")
   (setq arg (or arg 1))
   (when (/= arg 1)
@@ -125,3 +67,23 @@
     (when (= orig-point (point))
       (move-beginning-of-line 1))))
 (global-set-key [remap move-beginning-of-line] 'smarter-move-beginning-of-line)
+
+;; indent code right or left using C-Shift-right and C-Shift-left
+(defun shift-region (distance)
+  (let ((mark (mark)))
+    (save-excursion
+      (indent-rigidly (region-beginning) (region-end) distance)
+      (push-mark mark t t)
+      (setq deactivate-mark nil))))
+(defun shift-right ()
+  (interactive)
+  (shift-region 1))
+(defun shift-left ()
+  (interactive)
+  (shift-region -1))
+(global-set-key [C-S-right] 'shift-right)
+(global-set-key [C-S-left] 'shift-left)
+
+;; stop autosaving and creating backup files
+(setq make-backup-files nil)
+(setq auto-save-default nil)
